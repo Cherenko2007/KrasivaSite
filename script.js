b5b363a17a32e71ff00ee043bdf3003b0402fc35
Ulyana <Ucerenko@gmail.com>
2026-09-08 12:19:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -193,7 +193,7 @@ document.addEventListener('DOMContentLoaded', function () {
        МОДАЛЬНОЕ ОКНО ЗАПИСИ + ОТПРАВКА В TELEGRAM
        ============================================ */
 
-    const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzqDUjulkPscri2DBxzQ9z5_Yywg4cCIaebAZOz1210w6C7-0jx1XS4aNCww343Mzqg/exec';
+    const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw9uJOYk_ORmvBUft4wnmc9WjmVHR3z1M94y0ulQ1W5_RG2fOMD7tEG27s1xPCEw9g/exec';
 
     const modal = document.getElementById('bookingModal');
     const modalClose = document.getElementById('modalClose');
